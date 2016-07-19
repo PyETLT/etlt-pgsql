f2836e5f0fdd03f6b184e56b13b4fad89f336c74
Paul Water <p.r.water@setbased.nl>
2016-07-19 08:32:36 +0200

    Initial commit.

---
+++ etlt_pgsql/writer/PgSqlLoaderWriter.py
@@ -0,0 +1,121 @@
+"""
+ETLT-pgSQL
+
+Copyright 2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+import datetime
+import os
+
+import pytz
+from etlt.writer.SqlLoaderWriter import SqlLoaderWriter
+
+
+class PgSqlLoaderWriter(SqlLoaderWriter):
+    """
+    Writer for storing rows in CSV format optimized for PostgreSQL and 'copy from stdin' statement.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def write_date(value, file):
+        """
+        Writes a date as a field to a CSV file.
+
+        :param datetime.date value: The date.
+        :param T file: The file.
+        """
+        file.write(value.isoformat())
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def write_datetime(value, file):
+        """
+        Writes a datetime as a field to a CSV file.
+
+        :param datetime.datetime value: The date.
+        :param T file: The file.
+        """
+        zone = value.tzinfo
+        if zone and zone != pytz.utc:
+            raise ValueError('Only native and UTC timezone supported')
+        file.write(value.isoformat())
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def write_int(value, file):
+        """
+        Writes an integer as a field to a CSV file.
+
+        :param int value: The integer.
+        :param T file: The file.
+        """
+        file.write(str(value))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def write_none(_, file):
+        """
+        Writes None as a field to a CSV file.
+
+        :param None _: The None object.
+        :param T file: The file.
+        """
+        file.write('\\N')
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def write_string(value, file):
+        """
+        Writes a string as a field to a CSV file.
+
+        :param str value: The string.
+        :param T file: The file.
+        """
+        if value == '':
+            file.write('\\N')
+        else:
+            file.write("'")
+            file.write(value.replace('\\', '\\\\').replace("'", "\\'"))
+            file.write("'")
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def write(self, row):
+        """
+        Writes a row to the destination file.
+
+        :param dict[str,str|None] row: The row.
+        """
+        for i, field in enumerate(self._fields):
+            if i:
+                self._file.write(',')
+            self._write_field(row[field])
+
+        self._file.write(os.linesep)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_bulk_load_sql(self, table_name):
+        """
+        Returns a SQL statement for bulk loading the data into a table.
+
+        :param str table_name: The name of the table.
+
+        :rtype: str
+        """
+        sql = "copy {TABLE_NAME} from stdin csv"
+        sql += " delimiter ','"
+        sql += " null '\\N'"
+        sql += " quote '\'\''"
+        sql += " escape '\\'"
+        sql += " encoding '{ENCODING}'"
+
+        return sql.format(ENCODING=self._encoding, TABLE_NAME=table_name)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+PgSqlLoaderWriter.register_handler("<class 'int'>", PgSqlLoaderWriter.write_int)
+PgSqlLoaderWriter.register_handler("<class 'str'>", PgSqlLoaderWriter.write_string)
+PgSqlLoaderWriter.register_handler("<class 'NoneType'>", PgSqlLoaderWriter.write_none)
+PgSqlLoaderWriter.register_handler("<class 'datetime.date'>", PgSqlLoaderWriter.write_date)
+PgSqlLoaderWriter.register_handler("<class 'datetime.datetime'>", PgSqlLoaderWriter.write_datetime)
---
+++ test/writer/PgSqlLoaderWriterTest.py
@@ -0,0 +1,51 @@
+import datetime
+import unittest
+from os import path
+
+import pytz
+
+from etlt_pgsql.writer.PgSqlLoaderWriter import PgSqlLoaderWriter
+
+
+class PgSqlLoaderWriterTest(unittest.TestCase):
+    """
+    Test cases for PgSqlLoaderWriter.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_types(self):
+        filename_actual = path.abspath(path.dirname(__file__)) + '/PgSqlLoaderWriterTest/test_types.csv'
+        filename_expected = path.abspath(path.dirname(__file__)) + '/PgSqlLoaderWriterTest/test_types.expected.csv'
+
+        writer = PgSqlLoaderWriter(filename_actual)
+        writer.fields = ['int', 'str', 'none', 'empty', 'date', 'datetime']
+        rows = [{'int':   123,
+                 'str':   'Ministry of Silly Walks',
+                 'none':  None,
+                 'empty': '',
+                 'date':  datetime.date(1994, 1, 1),
+                 'datetime':  datetime.datetime(1994, 1, 1, 23, 15, 30)},
+                {'int':   123,
+                 'str':   'мỉאַîśŧґỷ өƒ Šỉŀłỷ שׂǻĺκŝ',  # https://www.tienhuis.nl/utf8-generator
+                 'none':  None,
+                 'empty': '',
+                 'date':  None,
+                 'datetime':  datetime.datetime(2016, 1, 1, 23, 15, 30, tzinfo=pytz.timezone('UTC'))}]
+
+        with writer:
+            for row in rows:
+                writer.write(row)
+
+        with open(filename_actual, 'rt', encoding='utf8') as file:
+            actual = file.read()
+
+        with open(filename_expected, 'rt', encoding='utf8') as file:
+            expected = file.read()
+
+        self.assertEqual(actual, expected)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #  @todo test strings with tabs and EOL
+    #  @todo test with lading and selecting data from actual PostgreSQL database
+
+# ------------------------------------------------------------------------------------------------------------------
